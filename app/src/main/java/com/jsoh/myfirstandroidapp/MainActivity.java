d0895bc6727baeeb0c4e27ae790b44e9c877ef7a
Junsuk Oh <a811219@gmail.com>
2016-02-03 13:28:56 +0900

    첫 번째 커밋

---
+++ app/src/main/java/com/jsoh/myfirstandroidapp/MainActivity.java
@@ -0,0 +1,17 @@
+
+package com.jsoh.myfirstandroidapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // 화면에 layout 표시
+        setContentView(R.layout.activity_main);
+
+    }
+}
