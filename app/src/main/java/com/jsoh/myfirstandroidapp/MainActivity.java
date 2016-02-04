5d2153577e7a38994ebf7a30a0c8b9c52cb1275c
Junsuk Oh <a811219@gmail.com>
2016-02-04 14:53:37 +0900

    button click event 2. onClickListener 구현

--- app/src/main/java/com/jsoh/myfirstandroidapp/MainActivity.java
+++ app/src/main/java/com/jsoh/myfirstandroidapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,10 +19,21 @@ public class MainActivity extends AppCompatActivity {
         // 화면에 layout 표시
         setContentView(R.layout.activity_main);
 
+        View.OnClickListener listener = new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "클릭 매우 잘 됨", Toast.LENGTH_SHORT).show();
+            }
+        };
+
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(listener);
+
     }
 
     // activity_main.xml 에 연결 됨
     public void onClick(View view) {
         Log.d(TAG, "클릭 잘 됨");
+        Toast.makeText(MainActivity.this, "클릭 잘 됨", Toast.LENGTH_SHORT).show();
     }
 }
