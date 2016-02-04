8562eb9941395efc48da47ef0ef3fd2cc4c5f2db
Junsuk Oh <a811219@gmail.com>
2016-02-04 14:35:46 +0900

    button click event 1. xml 에 onClick 속성 정의

--- app/src/main/java/com/jsoh/myfirstandroidapp/MainActivity.java
+++ app/src/main/java/com/jsoh/myfirstandroidapp/MainActivity.java
@@ -3,8 +3,12 @@ package com.jsoh.myfirstandroidapp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    public static final String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,5 +17,10 @@ public class MainActivity extends AppCompatActivity {
         // 화면에 layout 표시
         setContentView(R.layout.activity_main);
 
+    }
+
+    // activity_main.xml 에 연결 됨
+    public void onClick(View view) {
+        Log.d(TAG, "클릭 잘 됨");
     }
 }
