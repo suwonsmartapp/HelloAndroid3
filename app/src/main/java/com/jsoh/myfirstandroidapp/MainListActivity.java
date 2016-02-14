908e72fc1761115f8c7a25d07e1e1b226b020e3e
Junsuk Oh <a811219@gmail.com>
2016-02-14 22:31:15 +0900

    ListActivity 를 메인으로 변경

---
+++ app/src/main/java/com/jsoh/myfirstandroidapp/MainListActivity.java
@@ -0,0 +1,78 @@
+package com.jsoh.myfirstandroidapp;
+
+import android.app.ListActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.text.Collator;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 예제의 첫 화면
+ */
+public class MainListActivity extends ListActivity {
+
+    private final static Comparator<Map<String, Object>> sDisplayNameComparator =
+            new Comparator<Map<String, Object>>() {
+                private final Collator collator = Collator.getInstance();
+
+                public int compare(Map<String, Object> map1, Map<String, Object> map2) {
+                    return collator.compare(map1.get("title"), map2.get("title"));
+                }
+            };
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setListAdapter(new SimpleAdapter(this, getData(),
+                android.R.layout.simple_list_item_1, new String[] {
+                "title"
+        },
+                new int[] {
+                        android.R.id.text1
+                }));
+        getListView().setTextFilterEnabled(true);
+    }
+
+    protected List<Map<String, Object>> getData() {
+        List<Map<String, Object>> myData = new ArrayList<>();
+
+        // 메뉴 추가 부분
+        addItem(myData, "버튼 이벤트", MainActivity.class);
+        addItem(myData, "ScrollView", ScrollActivity.class);
+        // ----- 메뉴 추가 여기까지
+
+        // 이름 순 정렬
+        // Collections.sort(myData, sDisplayNameComparator);
+
+        return myData;
+    }
+
+    protected void addItem(List<Map<String, Object>> data, String name, Intent intent) {
+        Map<String, Object> temp = new HashMap<>();
+        temp.put("title", name);
+        temp.put("intent", intent);
+        data.add(temp);
+    }
+
+    protected void addItem(List<Map<String, Object>> data, String name, Class cls) {
+        this.addItem(data, name, new Intent(this, cls));
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        Map<String, Object> map = (Map<String, Object>) l.getItemAtPosition(position);
+
+        Intent intent = (Intent) map.get("intent");
+        startActivity(intent);
+    }
+}
