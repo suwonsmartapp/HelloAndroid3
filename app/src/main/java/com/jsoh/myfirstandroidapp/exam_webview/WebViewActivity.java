5673bc475ac55dda8f588f9d48d7e331bfd038cb
Junsuk Oh <a811219@gmail.com>
2016-02-16 11:54:08 +0900

    WebView 예제 : history 컨트롤

--- app/src/main/java/com/jsoh/myfirstandroidapp/exam_webview/WebViewActivity.java
+++ app/src/main/java/com/jsoh/myfirstandroidapp/exam_webview/WebViewActivity.java
@@ -5,6 +5,7 @@ import com.jsoh.myfirstandroidapp.R;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -36,5 +37,18 @@ public class WebViewActivity extends AppCompatActivity implements View.OnClickLi
     @Override
     public void onClick(View v) {
         mWebView.loadUrl(mEditAddress.getText().toString());
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        // Check if the key event was the Back button and if there's history
+        if ((keyCode == KeyEvent.KEYCODE_BACK) && mWebView.canGoBack()) {
+            mWebView.goBack();
+            return true;
+        }
+        // If it wasn't the Back key or there's no web page history, bubble up
+        // to the default
+        // system behavior (probably exit the activity)
+        return super.onKeyDown(keyCode, event);
     }
 }
