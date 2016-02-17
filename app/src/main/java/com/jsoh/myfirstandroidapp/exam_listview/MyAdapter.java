9cf796ed7579bb245db8dc2fc563aa0f403c997a
Junsuk Oh <a811219@gmail.com>
2016-02-17 16:44:51 +0900

    BaseAdapter를 상속한 커스텀 어댑터 예제 : ViewHolder 패턴 적용

--- app/src/main/java/com/jsoh/myfirstandroidapp/exam_listview/MyAdapter.java
+++ app/src/main/java/com/jsoh/myfirstandroidapp/exam_listview/MyAdapter.java
@@ -21,9 +21,13 @@ public class MyAdapter extends BaseAdapter {
     private Context mContext;
     private List<MyItem> mData;
 
+    private final LayoutInflater mInflater;
+
     public MyAdapter(Context context, List<MyItem> data) {
         mContext = context;
         mData = data;
+
+        mInflater = LayoutInflater.from(mContext);
     }
 
     // 아이템의 갯수
@@ -47,27 +51,44 @@ public class MyAdapter extends BaseAdapter {
     // 아이템 한 개의 View 를 완성 하는 곳
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
+        ViewHoler holder;
+
         if (convertView == null) {
             // 최초 로드 할 때
+            holder = new ViewHoler();
 
             // 레이아웃을 가져오게 하는 객체
-            LayoutInflater inflater = LayoutInflater.from(mContext);
-            convertView = inflater.inflate(R.layout.item_list, parent, false);
-        }
+            convertView = mInflater.inflate(R.layout.item_list, parent, false);
 
-        // 데이터를 연결 할 View
-        ImageView imageView = (ImageView) convertView.findViewById(R.id.imageview);
-        TextView title = (TextView) convertView.findViewById(R.id.title_text);
-        TextView description = (TextView) convertView.findViewById(R.id.description_text);
+            // 데이터를 연결 할 View
+            ImageView imageView = (ImageView) convertView.findViewById(R.id.imageview);
+            TextView title = (TextView) convertView.findViewById(R.id.title_text);
+            TextView description = (TextView) convertView.findViewById(R.id.description_text);
+
+            holder.imageView = imageView;
+            holder.title = title;
+            holder.description = description;
+
+            convertView.setTag(holder);
+        } else {
+            // 재활용
+            holder = (ViewHoler) convertView.getTag();
+        }
 
         // 데이터
         MyItem item = (MyItem) getItem(position);
 
         // 데이터 설정
-        imageView.setImageResource(item.getImageRes());
-        title.setText(item.getTitle());
-        description.setText(item.getDescription());
+        holder.imageView.setImageResource(item.getImageRes());
+        holder.title.setText(item.getTitle());
+        holder.description.setText(item.getDescription());
 
         return convertView;
+    }
+
+    private static class ViewHoler {
+        ImageView imageView;
+        TextView title;
+        TextView description;
     }
 }
