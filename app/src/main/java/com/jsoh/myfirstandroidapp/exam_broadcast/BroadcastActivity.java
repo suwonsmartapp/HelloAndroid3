3eff6ff04612ed2fd21ce67d3fc80162d94aa751
Junsuk Oh <a811219@gmail.com>
2016-02-25 10:56:27 +0900

    암시적 인텐트로 카카오에 메세지 전송, 특정 앱 실행

--- app/src/main/java/com/jsoh/myfirstandroidapp/exam_broadcast/BroadcastActivity.java
+++ app/src/main/java/com/jsoh/myfirstandroidapp/exam_broadcast/BroadcastActivity.java
@@ -4,9 +4,11 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import com.jsoh.myfirstandroidapp.R;
@@ -15,22 +17,47 @@ public class BroadcastActivity extends AppCompatActivity implements View.OnClick
     // 나만의 액션
     public static final String ACTION_MY = "com.jsoh.myfirstandroidapp.ACTION_MY";
     private MyReceiver mMyReceiver;
+    private EditText mMessageEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_broadcast);
 
+        mMessageEditText = (EditText) findViewById(R.id.message_edit);
+
         findViewById(R.id.button).setOnClickListener(this);
+        findViewById(R.id.kakao_send_button).setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        Intent intent = new Intent(ACTION_MY);
-
-        // 브로드캐스트 발송
-        sendBroadcast(intent);
+        switch (v.getId()) {
+            case R.id.button:
+                Intent intent = new Intent(ACTION_MY);
+                // 브로드캐스트 발송
+                sendBroadcast(intent);
+                break;
+            case R.id.kakao_send_button:
+                Intent kakaoIntent = new Intent(Intent.ACTION_SEND);
+                kakaoIntent.setPackage("com.kakao.talk");
+                kakaoIntent.setType("text/plain");
+                kakaoIntent.putExtra(Intent.EXTRA_TEXT, mMessageEditText.getText().toString());
+                if (kakaoIntent.resolveActivity(getPackageManager()) != null) {
+                    startActivity(kakaoIntent);
+                } else {
+                    Toast.makeText(BroadcastActivity.this, "카카오가 없어요", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
     }
+
+    // 특정 앱 설치 화면으로 이동
+    private void install() {
+        Intent intent1 = new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=com.squidtooth.gifplayer&hl=ko"));
+        startActivity(intent1);
+    }
+
 
     @Override
     protected void onResume() {
