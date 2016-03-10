aeb5e4872fa5d6e1d14e779f4b4a530b8e4f4fff
Junsuk Oh <a811219@gmail.com>
2016-03-10 11:00:22 +0900

    Facade 패턴을 활용한 insert

---
+++ app/src/main/java/com/jsoh/myfirstandroidapp/notepad/facade/MemoFacade.java
@@ -0,0 +1,49 @@
+package com.jsoh.myfirstandroidapp.notepad.facade;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+
+import com.jsoh.myfirstandroidapp.notepad.db.MemoContract;
+import com.jsoh.myfirstandroidapp.notepad.db.MemoDbHelper;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Memo 테이블에 대한 작업을 편하게 해주는 클래스
+ */
+public class MemoFacade {
+
+    private MemoDbHelper mHelper;
+    private SimpleDateFormat mSimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+    public MemoFacade(Context context) {
+        mHelper = new MemoDbHelper(context);
+    }
+
+    public long insertMemo(String title, String memo) {
+        // 쓰기 모드로 db 저장소를 얻기
+        SQLiteDatabase db = mHelper.getWritableDatabase();
+
+        // INSERT INTO Memo(title, memo, date) VALUES ('title', 'memo', '2000-10-10');
+        // db.execSQL("INSERT INTO Memo(title, memo, date) VALUES ('title', 'memo', '2000-10-10')");
+
+        String date = mSimpleDateFormat.format(new Date());
+
+        ContentValues values = new ContentValues();
+        if (title.length() != 0) {
+            values.put(MemoContract.MemoEntry.COLUMN_NAME_TITLE, title);
+        }
+        values.put(MemoContract.MemoEntry.COLUMN_NAME_MEMO, memo);
+        values.put(MemoContract.MemoEntry.COLUMN_NAME_DATE, date);
+
+        if (title.length() != 0 || memo.length() != 0) {
+            return db.insert(MemoContract.MemoEntry.TABLE_NAME,
+                    null,
+                    values);
+        }
+
+        return -1;
+    }
+}
--- app/src/main/java/com/jsoh/myfirstandroidapp/notepad/fragments/MemoEditFragment.java
+++ app/src/main/java/com/jsoh/myfirstandroidapp/notepad/fragments/MemoEditFragment.java
@@ -1,7 +1,5 @@
 package com.jsoh.myfirstandroidapp.notepad.fragments;
 
-import android.content.ContentValues;
-import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -12,21 +10,14 @@ import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
-import android.widget.Toast;
 
 import com.jsoh.myfirstandroidapp.R;
-import com.jsoh.myfirstandroidapp.notepad.db.MemoContract;
-import com.jsoh.myfirstandroidapp.notepad.db.MemoDbHelper;
-
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import com.jsoh.myfirstandroidapp.notepad.facade.MemoFacade;
 
 public class MemoEditFragment extends Fragment {
 
     private EditText mTitle;
     private EditText mMemo;
-
-    private SimpleDateFormat mSimpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
 
     @Nullable
     @Override
@@ -54,29 +45,7 @@ public class MemoEditFragment extends Fragment {
     public void onPause() {
         super.onPause();
 
-        // insert
-        MemoDbHelper dbHelper = new MemoDbHelper(getActivity());
-
-        // 쓰기 모드로 db 저장소를 얻기
-        SQLiteDatabase db = dbHelper.getWritableDatabase();
-
-        String title = mTitle.getText().toString();
-        String memo = mMemo.getText().toString();
-        String date = mSimpleDateFormat.format(new Date());
-
-        // INSERT INTO Memo(title, memo, date) VALUES ('title', 'memo', '2000-10-10');
-        // db.execSQL("INSERT INTO Memo(title, memo, date) VALUES ('title', 'memo', '2000-10-10')");
-
-        ContentValues values = new ContentValues();
-        values.put(MemoContract.MemoEntry.COLUMN_NAME_TITLE, title);
-        values.put(MemoContract.MemoEntry.COLUMN_NAME_MEMO, memo);
-        values.put(MemoContract.MemoEntry.COLUMN_NAME_DATE, date);
-
-        if (db.insert(MemoContract.MemoEntry.TABLE_NAME,
-                null,
-                values) == -1) {
-            Toast.makeText(
-                    getActivity(), "에러!!!", Toast.LENGTH_SHORT).show();
-        }
+        MemoFacade facade = new MemoFacade(getActivity());
+        facade.insertMemo(mTitle.getText().toString(), mMemo.getText().toString());
     }
 }
