a895547cc7d764c37215f68d90aad3e794bad414
Junsuk Oh <a811219@gmail.com>
2016-03-10 10:43:01 +0900

    DB 모니터링 툴 : Stetho

---
+++ app/src/main/java/com/jsoh/myfirstandroidapp/MyApplication.java
@@ -0,0 +1,17 @@
+package com.jsoh.myfirstandroidapp;
+
+import android.app.Application;
+
+import com.facebook.stetho.Stetho;
+
+/**
+ * Created by junsuk on 16. 3. 10..
+ */
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        Stetho.initializeWithDefaults(this);
+    }
+}
