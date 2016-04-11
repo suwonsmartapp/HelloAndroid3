2c837ab8502e020672f564ca2fb3c3d49328eee2
Junsuk Oh <a811219@gmail.com>
2016-04-11 11:44:38 +0900

    GCM 적용

--- app/src/main/java/com/jsoh/myfirstandroidapp/smooch_slack_whispers_chatting/SuperChatActivity.java
+++ app/src/main/java/com/jsoh/myfirstandroidapp/smooch_slack_whispers_chatting/SuperChatActivity.java
@@ -2,7 +2,11 @@ package com.jsoh.myfirstandroidapp.smooch_slack_whispers_chatting;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
+import com.jsoh.myfirstandroidapp.R;
+
+import io.smooch.core.Smooch;
 import io.smooch.ui.ConversationActivity;
 
 public class SuperChatActivity extends AppCompatActivity {
@@ -10,6 +14,15 @@ public class SuperChatActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_super_chat);
+
+        findViewById(R.id.gcm).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Smooch.track("gcm test");
+            }
+        });
 
         ConversationActivity.show(this);
     }
