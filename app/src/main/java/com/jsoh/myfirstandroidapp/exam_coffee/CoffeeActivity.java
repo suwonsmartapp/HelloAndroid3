436cea35a220710aad8f71396ec325b637e5ce39
Junsuk Oh <a811219@gmail.com>
2016-04-18 11:45:03 +0900

    DataBinding

--- app/src/main/java/com/jsoh/myfirstandroidapp/exam_coffee/CoffeeActivity.java
+++ app/src/main/java/com/jsoh/myfirstandroidapp/exam_coffee/CoffeeActivity.java
@@ -1,6 +1,7 @@
 
 package com.jsoh.myfirstandroidapp.exam_coffee;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.Editable;
@@ -11,11 +12,9 @@ import android.view.ViewGroup;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.view.animation.CycleInterpolator;
-import android.widget.CheckBox;
-import android.widget.EditText;
-import android.widget.TextView;
 
 import com.jsoh.myfirstandroidapp.R;
+import com.jsoh.myfirstandroidapp.databinding.ActivityCoffeeBinding;
 import com.jsoh.myfirstandroidapp.utils.RandomAnimationUtil;
 
 public class CoffeeActivity extends AppCompatActivity implements View.OnClickListener {
@@ -24,16 +23,19 @@ public class CoffeeActivity extends AppCompatActivity implements View.OnClickLis
 
     private int mQuantity = 0;
 
-    private CheckBox mCheckCream;
-    private EditText mEtCustomerName;
-    private TextView mTvQuantity;
-    private TextView mTvSummary;
+//    private CheckBox mCheckCream;
+//    private EditText mEtCustomerName;
+//    private TextView mTvQuantity;
+//    private TextView mTvSummary;
     private Animation mShakeAnimation;
+
+    ActivityCoffeeBinding mBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_coffee);
+//        setContentView(R.layout.activity_coffee);
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_coffee);
 
         View rootView = ((ViewGroup)findViewById(android.R.id.content)).getChildAt(0);
         RandomAnimationUtil.randomAnimantionStart(this, rootView);
@@ -41,9 +43,7 @@ public class CoffeeActivity extends AppCompatActivity implements View.OnClickLis
         mShakeAnimation = AnimationUtils.loadAnimation(this, R.anim.shake);
         mShakeAnimation.setInterpolator(new CycleInterpolator(20));
 
-        mCheckCream = (CheckBox) findViewById(R.id.cream_check);
-        mEtCustomerName = (EditText) findViewById(R.id.customer_name_edit);
-        mEtCustomerName.addTextChangedListener(new TextWatcher() {
+        mBinding.customerNameEdit.addTextChangedListener(new TextWatcher() {
             @Override
             public void beforeTextChanged(CharSequence s, int start, int count, int after) {
 
@@ -51,7 +51,7 @@ public class CoffeeActivity extends AppCompatActivity implements View.OnClickLis
 
             @Override
             public void onTextChanged(CharSequence s, int start, int before, int count) {
-                mEtCustomerName.startAnimation(mShakeAnimation);
+                mBinding.customerNameEdit.startAnimation(mShakeAnimation);
             }
 
             @Override
@@ -59,13 +59,11 @@ public class CoffeeActivity extends AppCompatActivity implements View.OnClickLis
 
             }
         });
-        mTvQuantity = (TextView) findViewById(R.id.quantity_text);
-        mTvSummary = (TextView) findViewById(R.id.summary_text);
 
         // 버튼들 이벤트
-        findViewById(R.id.minus_btn).setOnClickListener(this);
-        findViewById(R.id.plus_btn).setOnClickListener(this);
-        findViewById(R.id.order_btn).setOnClickListener(this);
+        mBinding.minusBtn.setOnClickListener(this);
+        mBinding.plusBtn.setOnClickListener(this);
+        mBinding.orderBtn.setOnClickListener(this);
     }
 
     @Override
@@ -88,14 +86,14 @@ public class CoffeeActivity extends AppCompatActivity implements View.OnClickLis
                 if (mQuantity < 0) {
                     mQuantity = 0;
                 }
-                mTvQuantity.setText("" + mQuantity);
+                mBinding.quantityText.setText("" + mQuantity);
                 break;
             case R.id.plus_btn:
                 Animation ani = AnimationUtils.loadAnimation(this, R.anim.rotate2);
                 ani.setInterpolator(new CycleInterpolator(10));
                 v.startAnimation(ani);
                 mQuantity++;
-                mTvQuantity.setText("" + mQuantity);
+                mBinding.quantityText.setText("" + mQuantity);
                 break;
             case R.id.order_btn:
                 printSummary();
@@ -107,16 +105,16 @@ public class CoffeeActivity extends AppCompatActivity implements View.OnClickLis
     private void printSummary() {
         int totalPrice = COFFEE_PRICE * mQuantity;
 
-        if (mCheckCream.isChecked()) {
+        if (mBinding.creamCheck.isChecked()) {
             totalPrice += CREAM_PRICE;
         }
 
         StringBuilder sb = new StringBuilder("주문요약");
-        sb.append("\n이름 : ").append(mEtCustomerName.getText().toString());
+        sb.append("\n이름 : ").append(mBinding.customerNameEdit.getText().toString());
         sb.append("\n수량 : ").append(mQuantity);
-        sb.append("\n휘핑크림 추가 여부 : ").append(mCheckCream.isChecked());
+        sb.append("\n휘핑크림 추가 여부 : ").append(mBinding.creamCheck.isChecked());
         sb.append("\n합계 : ").append(totalPrice);
 
-        mTvSummary.setText(sb);
+        mBinding.summaryText.setText(sb);
     }
 }
